ab6d9753b3ee7fc069acbc5d4d656a105e9970ca
negar <negar.karimnejad@gmail.com>
2024-02-18 19:05:07 +0330

    coding

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from "next";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import { cache } from "react";
+import AddToCartButton from "./AddToCartButton";
 
 interface ProductPageProps {
   params: { id: string };
@@ -48,23 +49,8 @@ async function ProductPage({ params: { id } }: ProductPageProps) {
             <div className="badge">{formatCurrency(product.price)}</div>
           </div>
           <p className="py-6">{product.description}</p>
-          <button className="btn btn-primary">
-            Add To Cart
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
-          </button>
+
+          <AddToCartButton id={product.id} />
         </div>
       </div>
     </div>
---
+++ src/lib/cart.ts
@@ -0,0 +1,6 @@
+"use server";
+import prisma from "@/lib/prismadb";
+
+export async function AddToCart(id: string) {
+
+}
