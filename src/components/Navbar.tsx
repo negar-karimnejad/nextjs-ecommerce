d55ce37ab8a26955e0cfd9fa9c242f4aa54b168c
negar <negar.karimnejad@gmail.com>
2024-02-19 19:56:08 +0330

    pass the cart details to navbar cart icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,53 +2,36 @@ import Image from "next/image";
 import Link from "next/link";
 import Logo from "../assets/logo.png";
 import Input from "./ui/Input";
+import { redirect } from "next/navigation";
+import ShoppingCartButton from "./ShoppingCartButton";
+import { getCart } from "@/lib/cart";
 
-function Navbar() {
+async function searchProducts(formData: FormData) {
+  "use server";
+
+  const searchQuery = formData.get("searchQuery")?.toString();
+  if (searchQuery) {
+    redirect(`/search?query=${searchQuery}`);
+  }
+}
+
+async function Navbar() {
+  const cart = await getCart();
   return (
     <div className="navbar bg-base-100 md:px-14">
       <div className="flex-1">
-        <Link href={"/"} className="btn btn-ghost text-xl">
+        <Link href="/" className="btn btn-ghost text-xl">
           <Image src={Logo} alt="" width={30} height={30} />
           <h1 className="font-bold text-xl hidden sm:block">Flowmazon</h1>
         </Link>
       </div>
       <div className="max-w-40 sm:max-w-xl">
-        <Input type={"text"} placeholder={"Search"} />
+        <form action={searchProducts}>
+          <Input type={"text"} placeholder={"Search"} name="searchQuery" />
+        </form>
       </div>
       <div className="flex-none mx-2">
-        <div className="dropdown dropdown-end">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              <span className="badge badge-sm indicator-item">8</span>
-            </div>
-          </div>
-          <div
-            tabIndex={0}
-            className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
-          >
-            <div className="card-body">
-              <span className="font-bold text-lg">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <ShoppingCartButton cart={cart} />
       </div>
 
       <div className="dropdown dropdown-end">
