291ea32101a9e1485702a9551ae94d36d033db56
negar <negar.karimnejad@gmail.com>
2024-02-19 23:01:01 +0330

    handle product quantity

--- src/app/cart/CartEntry.tsx
+++ src/app/cart/CartEntry.tsx
@@ -4,12 +4,28 @@ import { CartItemWithProduct } from "@/lib/cart";
 import { formatCurrency } from "@/lib/formatCurrency";
 import Image from "next/image";
 import Link from "next/link";
+import { useTransition } from "react";
 
 interface CartEntryProps {
   cartItem: CartItemWithProduct;
+  setProductQuantity: (productId: string, quantity: number) => Promise<void>;
 }
 
-function CartEntry({ cartItem: { product, quantity } }: CartEntryProps) {
+function CartEntry({
+  cartItem: { product, quantity },
+  setProductQuantity,
+}: CartEntryProps) {
+  const [isPending, startTransition] = useTransition();
+
+  const quantityOptions: JSX.Element[] = [];
+  for (let i = 1; i <= 99; i++) {
+    quantityOptions.push(
+      <option key={i} value={i}>
+        {i}
+      </option>
+    );
+  }
+
   return (
     <>
       <div className="card card-side">
@@ -28,21 +44,28 @@ function CartEntry({ cartItem: { product, quantity } }: CartEntryProps) {
             {product.name}
           </Link>
           <div>Price: {formatCurrency(product.price)}</div>
-          <form action="">
-            <label htmlFor="">Quantity:</label>
+          <div className="my-1 flex items-center gap-2">
+            Quantity:
             <select
               defaultValue={quantity}
-              onChange={() => {}}
-              className="ml-2 p-2 rounded-md"
+              onChange={(e) => {
+                const newQuantity = parseInt(e.currentTarget.value);
+                startTransition(async () => {
+                  await setProductQuantity(product.id, newQuantity);
+                });
+              }}
+              className="select select-bordered w-full max-w-[80px]"
             >
-              {Array.from({ length: 99 }, (_, i) => (
-                <option key={i} value={i}>
-                  {i}
-                </option>
-              ))}
+              <option value={0}>0 (Remove)</option>
+              {quantityOptions}
             </select>
-          </form>
-          <p>Total: {formatCurrency(quantity * product.price)}</p>
+          </div>
+          <div className="flex items-center gap-2 w-fit">
+            <p>Total: {formatCurrency(quantity * product.price)}</p>
+            {isPending && (
+              <span className="loading loading-spinner loading-md" />
+            )}
+          </div>
         </div>
       </div>
       <div className="divider"></div>
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,6 +2,7 @@ import { getCart } from "@/lib/cart";
 import { formatCurrency } from "@/lib/formatCurrency";
 import Image from "next/image";
 import CartEntry from "./CartEntry";
+import { setProductQuantity } from "./actions";
 
 export const metadata = {
   title: "Your Cart - Flowmazon",
@@ -16,14 +17,21 @@ async function CartPage() {
       {/* cart products */}
       <div className="flex flex-col gap-2">
         {cart?.items.map((cartItem) => (
-          <CartEntry key={cartItem.id} cartItem={cartItem} />
+          <CartEntry
+            key={cartItem.id}
+            cartItem={cartItem}
+            setProductQuantity={setProductQuantity}
+          />
         ))}
       </div>
-      <div className="mx-auto text-center">
+      {!cart?.items.length && (
+        <p className="font-medium">Your cart is empty.</p>
+      )}
+      <div className="flex flex-col items-end sm:items-center">
         <p className="font-bold mt-4 mb-2">
           Total: {formatCurrency(cart?.subtotal || 0)}
         </p>
-        <button className="btn btn-warning px-5">CHECKOUT</button>
+        <button className="btn btn-warning sm:w-[200px]">CHECKOUT</button>
       </div>
     </div>
   );
