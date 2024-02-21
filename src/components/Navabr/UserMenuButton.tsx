98034e40107d117ebf2994523df10ca22693c599
negar <negar.karimnejad@gmail.com>
2024-02-21 19:00:08 +0330

    coding

--- src/components/Navabr/UserMenuButton.tsx
+++ src/components/Navabr/UserMenuButton.tsx
@@ -24,19 +24,9 @@ function UserMenuButton({ session }: UserMenuButtonProps) {
           />
         ) : (
           <>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-5 h-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>
-            </svg>
+            <span className="text-xl inline-block w-5 h-5 mb-4 stroke-current">
+              ...
+            </span>
           </>
         )}
       </button>
