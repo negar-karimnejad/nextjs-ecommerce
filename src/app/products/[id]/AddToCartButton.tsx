9bc39b98a3b02e579d567d866850d5906fcc605c
negar <negar.karimnejad@gmail.com>
2024-02-21 19:17:31 +0330

    fixed icon bug

--- src/app/products/[id]/AddToCartButton.tsx
+++ src/app/products/[id]/AddToCartButton.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import Image from "next/image";
 import { useState, useTransition } from "react";
+import shoppingCartIcon from "../../../assets/shopping-cart.png";
 
 interface AddToCartButtonProps {
   productId: string;
@@ -27,20 +29,13 @@ function AddToCartButton({
         className="btn btn-primary"
       >
         Add To Cart
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-          />
-        </svg>
+        <Image
+          src={shoppingCartIcon}
+          alt="shopping-cart"
+          width={20}
+          height={20}
+          className="w-5 h-5"
+        />
       </button>
       {isPending && <span className="loading loading-spinner loading-md" />}
       {!isPending && success && (
