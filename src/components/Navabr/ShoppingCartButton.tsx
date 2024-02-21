1230d973750d6e45c7525c6d0c8d99d69d299bce
negar <negar.karimnejad@gmail.com>
2024-02-21 18:49:32 +0330

    add shopping cart png

--- src/components/Navabr/ShoppingCartButton.tsx
+++ src/components/Navabr/ShoppingCartButton.tsx
@@ -2,7 +2,9 @@
 
 import { ShoppingCart } from "@/lib/cart";
 import { formatCurrency } from "@/lib/formatCurrency";
+import Image from "next/image";
 import Link from "next/link";
+import shoppingCartIcon from "../../assets/shopping-cart.png";
 
 interface ShoppingCartButtonProps {
   cart: ShoppingCart | null;
@@ -15,28 +17,22 @@ function ShoppingCartButton({ cart }: ShoppingCartButtonProps) {
     }
   };
   return (
-    <div className="dropdown dropdown-end flex-none mx-2">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+    <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn-ghost btn-circle btn">
         <div className="indicator">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-            />
-          </svg>
+          <Image
+            src={shoppingCartIcon}
+            alt="shopping-cart"
+            width={20}
+            height={20}
+            className="w-5 h-5"
+          />
           <span className="badge badge-sm indicator-item">
             {cart?.size || 0}
           </span>
         </div>
-      </div>
+      </label>
+
       <div
         tabIndex={0}
         className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
